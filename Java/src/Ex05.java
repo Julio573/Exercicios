bb7ad3c6d6ddbc0d5472ef87896170faaf7ba51f
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 10:47:09 -0300

    Adicionando exercicio 05

---
+++ Java/src/Ex05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex05 {
+    public static void main(String[] args) {
+
+        int codigoPeça1, codigoPeça2, quantidade1, quantidade2;
+        double valorPeça1, valorPeça2, valorTotal;
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        codigoPeça1 = scan.nextInt();
+        quantidade1 = scan.nextInt();
+        valorPeça1 = scan.nextDouble();
+
+        codigoPeça2 = scan.nextInt();
+        quantidade2 = scan.nextInt();
+        valorPeça2 = scan.nextDouble();
+
+        valorTotal = (quantidade1 * valorPeça1) + (quantidade2 * valorPeça2);
+
+        System.out.printf("Valor a pagar: R$%.2f%n", valorTotal);
+
+
+
+        scan.close();
+    }
+}
