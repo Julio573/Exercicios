30cec7a8d7fb7d0f28a5a7daa636bac51e9832dc
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 11:00:26 -0300

    Atualizando

---
+++ Java/src/ex06.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ex06 {
+    public static void main(String[] args) {
+
+        double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        A = scan.nextDouble();
+        B = scan.nextDouble();
+        C = scan.nextDouble();
+
+        triangulo = A * C / 2;
+        circulo = 3.14159 * Math.pow(C, 2);
+        trapezio = (A + B) * C / 2;
+        quadrado = Math.pow(B, 2);
+        retangulo = A * B;
+
+        System.out.printf("Triângulo: %.3f%n", triangulo);
+        System.out.printf("Círculo: %.3f%n", circulo);
+        System.out.printf("Trapézio: %.3f%n", trapezio);
+        System.out.printf("Quadrado: %.3f%n", quadrado);
+        System.out.printf("Retângulo: %.3f%n", retangulo);
+
+        scan.close();
+    }
+}
