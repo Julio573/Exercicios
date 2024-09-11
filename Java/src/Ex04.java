89ac5dc2fa65b41382c6f4bc21c742e4523e9181
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 10:36:01 -0300

    Adicionando exercicio 04

---
+++ Java/src/Ex04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex04 {
+    public static void main(String[] args) {
+
+        int numeroFuncionario;
+        double horasTrabalhadas, valorHora, salario;
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        numeroFuncionario = scan.nextInt();
+        horasTrabalhadas = scan.nextDouble();
+        valorHora = scan.nextDouble();
+
+        salario = valorHora * horasTrabalhadas;
+
+        System.out.println("Número de funcionário: " + numeroFuncionario);
+        System.out.printf("Salário = U$%.2f%n", salario);
+
+        scan.close();
+    }
+}
