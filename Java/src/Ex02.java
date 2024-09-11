efc10bb42c1925f32d39ddee1d22740915c5404f
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 10:08:09 -0300

    Inserindo segundo exercicio

---
+++ Java/src/Ex02.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        double raio, area;
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        raio = scan.nextDouble();
+        area = 3.14159 * Math.pow(raio, 2);
+        System.out.printf("A= %.4f", area);
+
+        scan.close();
+    }
+}
