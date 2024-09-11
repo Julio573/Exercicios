09b995a83e76bd75014f108f85da6586a2035767
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 09:53:50 -0300

    Adicionando exercicio 1

--- Java/src/Ex01.java
+++ Java/src/Ex01.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Ex01 {
     public static void main(String[] args) {
 
+        int valor1, valor2, resultado;
+
+        Scanner scan = new Scanner(System.in);
+
+        valor1 = scan.nextInt();
+        valor2 = scan.nextInt();
+        resultado = valor1 + valor2;
+
+        System.out.println("SOMA = " + resultado);
+
+        scan.close();
     }
 }
