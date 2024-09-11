f5bcc5b2d7f424bf85ccad5729a1e5330aae2fbc
Julio Peixoto <julio8497@hotmail.com>
2024-09-11 10:24:18 -0300

    Adicionando exercicio 03

---
+++ Java/src/Ex03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        int A, B, C, D, diferença;
+
+        Scanner scan = new Scanner(System.in);
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+        D = scan.nextInt();
+        diferença = (A * B) - (C * D);
+
+        System.out.println("Diferença = " + diferença);
+
+        scan.close();
+    }
+}
