95115a199922cdb2e7c3b924fbad37b2fb3292d8
Julio Peixoto <julio8497@hotmail.com>
2024-09-12 07:00:39 -0300

    Adicionando exercicio 07

--- Java/src/Ex07.java
+++ Java/src/Ex07.java
@@ -1,7 +1,21 @@
-
+import java.util.Scanner;
 
 public class Ex07 {
     public static void main(String[] args) {
+
+        int numero;
+
+        Scanner scan = new Scanner(System.in);
+
+        numero = scan.nextInt();
+
+        if (numero >= 0) {
+            System.out.println(numero + " é um número positivo.");
+        } else {
+            System.out.println(numero + " é um número negativo.");
+        }
+
+        scan.close();
 
     }
 }
